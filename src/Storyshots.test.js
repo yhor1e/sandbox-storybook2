4ad9b54fb5841e605df83874fae7ecef6c97ec2e
yhorie <yuk.horie@gmail.com>
2020-10-02 13:33:33 +0900

    Chgane __snapshot__ directory

--- src/Storyshots.test.js
+++ src/Storyshots.test.js
@@ -1,3 +1,16 @@
-import initStoryshots from "@storybook/addon-storyshots";
+//import initStoryshots, { imageSnapshot } from "@storybook/addon-storyshots";
+//
+//initStoryshots({ suite: "Image storyshots", test: imageSnapshot() });
 
-initStoryshots();
+import initStoryshots, {
+  Stories2SnapsConverter,
+  multiSnapshotWithOptions,
+} from "@storybook/addon-storyshots";
+
+initStoryshots({
+  test: multiSnapshotWithOptions(),
+  stories2snapsConverter: new Stories2SnapsConverter({
+    snapshotsDirName: "./__snapshots__",
+    snapshotExtension: ".snap.js",
+  }),
+});
